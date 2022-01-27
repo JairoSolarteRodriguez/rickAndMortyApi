4ca8e253b1ad598a8630b8c896613cc8920c2ab5
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-01-27 12:27:35 -0500

    corregido el renderizado condicional

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -8,8 +8,8 @@ export const Character = ({id, name, species, status, gender, image}) => {
       <img src={image} alt={name} />
       <div className='info'>
         <p><span className='bold-text'>Especie: </span>{species}</p>
-        <p className='flex-estado'><span className='bold-text'>Estado:</span> <span className={status === 'Alive'? 'alive' : 'dead'}></span>{status === 'Alive' ? 'Vivo' : 'Muerto'}</p>
-        <p><span className='bold-text'>Genero: </span>{gender === 'Male' ? 'Masculino' : 'Femenino'}</p>
+        <p className='flex-estado'><span className='bold-text'>Estado:</span> <span className={status === 'Alive'? 'alive' : status === 'unknown' ? 'unknown' : 'dead'}></span>{status}</p>
+        <p><span className='bold-text'>Genero: </span>{gender}</p>
       </div>
     </section>
   )
